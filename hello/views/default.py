15e05a02c2ad716181595c04fb7d6657ba23c000
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-07-17 03:10:32 +0100

    a

--- hello/views/default.py
+++ hello/views/default.py
@@ -9,6 +9,7 @@ from .. import models
 def home_desktop(request):
     return {"status": "This is the homepage for desktop devices"}
 
+
 @view_config(route_name="home", renderer="json", device="mobile")
 def home_mobile(request):
     return {"status": "This is the homepage for mobile devices"}
