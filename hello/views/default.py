e9a316e65d38822ed6b5edbee7a90d922ac24e6c
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-07-17 03:09:10 +0100

    space

--- hello/views/default.py
+++ hello/views/default.py
@@ -5,7 +5,6 @@ from sqlalchemy.exc import DBAPIError
 
 from .. import models
 
-
 @view_config(route_name="home", renderer="json")
 def home_desktop(request):
     return {"status": "This is the homepage for desktop devices"}
