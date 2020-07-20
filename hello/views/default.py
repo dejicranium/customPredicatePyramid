3fdbeeff68e0147a85db5584696ad6d5e2863390
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-07-20 10:50:11 +0100

    a

--- hello/views/default.py
+++ hello/views/default.py
@@ -10,7 +10,8 @@ def home_desktop(request):
     return {"status": "This is the homepage for desktop devices"}
 
 
+
 @view_config(route_name="home", renderer="json", device="mobile")
 def home_mobile(request):
-    
+
     return {"status": "This is the homepage for mobile devices"}
